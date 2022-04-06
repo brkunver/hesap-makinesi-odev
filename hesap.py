f594d689929e4c2b58b75c26007be9468663222f
burakhaunver <burakhanunver@gmail.com>
2022-04-06 16:35:22 +0300

    versiyon3

--- hesap.py
+++ hesap.py
@@ -1,18 +1,24 @@
-tercih = input("toplama icin 1 , bolme icin 2 ye basiniz : ")
-
-
+#Burakhan unver 18253066
 def toplama():
     x1 = input("birinci sayiyi giriniz : ")
     x2 = input("birinci sayiyi giriniz : ")
+    
     print("toplam : " , int(x1) + int(x2) )
 
 
 def bolme():
     x1 = input("birinci sayiyi giriniz : ")
     x2 = input("birinci sayiyi giriniz : ")
-    print("toplam : " , int(x1) / int(x2) )
+    if(int(x2) == 0):
+        print("lutfen gecerli bir sayi giriniz")
+        bolme()
+    else:
+        2
+        print("toplam : " , int(x1) / int(x2) )
 
-    
+
+tercih = input("toplama icin 1 , bolme icin 2 ye basiniz : ")
+
 if int(tercih) == 1:
     toplama()
 if int(tercih) == 2:
